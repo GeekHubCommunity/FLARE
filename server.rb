079ecd729d5b71268c1a51bff4c0473a71e06e38
GeekHubCommunity <128222991+GeekHubCommunity@users.noreply.github.com>
2023-03-18 17:25:07 +0100

    Update server.rb

--- server.rb
+++ server.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 require 'rest-client'
 require 'json'
 
-CLIENT_ID = ENV[I6QKAJ8Zk5wQ0mRYPpvGt0sl9VzfeKGwojBd0cLTGQ7xEQtq]
-CLIENT_SECRET = ENV[eX6tGIYxeikPxEpa3totgjos6NRJoZlzJlRyFvGXdbxXHudPDgiOWrviTNFIzlFL]
+CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
+CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
 
 get '/' do
   erb :index, :locals => {:client_id => CLIENT_ID}
